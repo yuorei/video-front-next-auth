cf6581567695286bd2f65c32ccd63a24a7f0cc0f
yuorei <108039575+yuorei@users.noreply.github.com>
2024-01-26 00:06:51 +0900

    all video

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,11 @@
-import CustomLink from "@/components/custom-link"
-import packageJSON from "../package.json"
-import HLSPlayer from '@/components/HLSPlayer';
+"use client";
+import type { Session } from "next-auth";
+import AllVideo from "@/components/all-video";
 
-export default function Index() {
-  const videoSrc = 'http://localhost:9000/video-service/output_video_2237e174-a98e-11ee-a8a8-6a6798be1bd7.m3u8';
+export default function Index({ session }: { session: Session }) {
   return (
-    <div className="space-y-2">
-      <h1 className="text-3xl font-bold">NextAuth.js Example</h1>
-      <p>
-        This is an example site to demonstrate how to use{" "}
-        <CustomLink href="https://nextjs.authjs.dev">NextAuth.js</CustomLink>{" "}
-        for authentication. Check out the{" "}
-        <CustomLink href="/server-example" className="underline">
-          Server
-        </CustomLink>{" "}
-        and the{" "}
-        <CustomLink href="/client-example" className="underline">
-          Client
-        </CustomLink>{" "}
-        examples to see how to secure pages and get session data.
-      </p>
-      <p>
-        Current{" "}
-        <CustomLink href="https://nextjs.authjs.dev">NextAuth.js</CustomLink>{" "}
-        version: <em>next-auth@{packageJSON.dependencies["next-auth"]}</em>
-      </p>
-      <HLSPlayer src={videoSrc} />
-    </div>
-  )
+    <main className="max-w-full">
+      <AllVideo />
+    </main>
+  );
 }
