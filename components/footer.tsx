9040a620d40a8c56dd811bce145ece5495f13df0
yuorei <108039575+yuorei@users.noreply.github.com>
2024-01-28 21:00:43 +0900

    fix

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,7 +2,7 @@ import CustomLink from "./custom-link"
 
 export default function Footer() {
   return (
-    <footer className="flex flex-col w-full px-4 mx-0 my-4 space-y-1 text-sm md:max-w-3xl md:my-12 md:mx-auto sm:px-6 md:h-5 md:items-center md:space-y-0 md:space-x-4 md:flex-row">
+    <footer className="flex flex-col justify-center w-full px-4 mx-0 my-4 space-y-1 text-sm md:max-w-3xl md:my-12 md:mx-auto sm:px-6 md:h-5 md:items-center md:space-y-0 md:space-x-4 md:flex-row">
       <CustomLink href="https://nextjs.authjs.dev">Documentation</CustomLink>
       <CustomLink href="https://www.npmjs.com/package/next-auth">
         NPM
